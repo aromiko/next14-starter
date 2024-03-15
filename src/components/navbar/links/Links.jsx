ad45a55c7727a0deac56f09d492f0417aec14db5
aromiko <mikoaro1212@gmail.com>
2024-03-15 14:15:34 +0800

    Designing of pages and components

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import Image from 'next/image';
+import NavLink from './navLink/NavLink';
+import styles from './links.module.css';
+import { useState } from 'react';
+
+const Links = () => {
+  const [open, setOpen] = useState(false);
+  const links = [
+    { title: 'Homepage', path: '/' },
+    { title: 'About', path: '/about' },
+    { title: 'Contact', path: '/contact' },
+    { title: 'Blog', path: '/blog' },
+  ];
+
+  const session = true;
+  const isAdmin = true;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: 'Admin', path: '/admin' }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: 'Login', path: '/login' }} />
+        )}
+      </div>
+      <Image
+        src="/menu.png"
+        alt="menu"
+        className={styles.menuButton}
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Links;
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './postCard.module.css';
+
+const PostCard = ({ post }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.imgContainer}>
+          <Image src="/noavatar.png" alt="" fill className={styles.img} />
+        </div>
+        <span className={styles.date}>01.01.2021</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>Desc</p>
+        <Link className={styles.link} href="/blog/post">
+          READ MORE
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
